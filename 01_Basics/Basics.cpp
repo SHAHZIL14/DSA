e30d5205c734cfcbcf86470e47786bd492b53d8d
Mohd Shazil Raza <shazilraza8@gmail.com>
2026-03-24 15:48:42 +0530

    Basics of c++ programing .

---
+++ 01_Basics/Basics.cpp
@@ -0,0 +1,44 @@
+#include<iostream> // inlcuding input/output stream //
+using namespace std; // Using standard namespace //
+
+int main(){ // Main function that gets executed on run //
+
+  // Data Types->
+  // Integer-> int 'name' To store the integer values;
+  // float-> float 'name' To store decimal values;
+  // Long-> long 'name' To store large integer values ;
+  // Long Long-> long long 'name'  To store Extra large values;
+  // Double-> double 'name' To store larger decimal values;
+  // Char-> char 'name' To store any character;
+  // String -> string 'name' Collection of characters or sentence
+  
+  // I/O->
+  // cin - > To take input
+  // cout - > To give output or print on screen
+
+
+  // Functions -> Functions are used to modularize the code so that 
+  // instead of writing the same bunch of code line again and again we
+  // define the bunch and call it again and again throughout the code 
+
+  // Syntax - > 'data type' 'name'(input variables){
+  //  'operation on variables'
+  //  return 'data type' 
+  // }
+
+  // Types->
+  // 1. Void function - > Does not return any data when called
+  // 2. Parameterised function - > Takes one or more parameters to perform
+  // operations on data
+  // 3.Non-parameterised function - > Does not require any parameter to 
+  // to perform operations
+  
+  // For example -> Function to add two integers->
+  // int sum(int a, int b){
+  //   int c =  a + b;
+  //   return c
+  // }
+
+  cout<<"Hello world";
+  return 0;
+}
