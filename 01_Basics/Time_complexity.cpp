b6ad166b2ea19392691bca79e765aa418421530e
Mohd Shazil Raza <shazilraza8@gmail.com>
2026-03-25 11:20:50 +0530

    Added Time Complexity and Space Complexity

---
+++ 01_Basics/Time_complexity.cpp
@@ -0,0 +1,26 @@
+// TIME COMPLEXITY -> The rate of change of time to execute the program 
+// according to the  the hardware and software of the system.
+
+// Notation - >
+// Big O -> O(variable or constant) = Worst case complexity.
+// Theta 0 - > 0(variable or constant) = average case complexity.
+// Omega -> omega(variable or constant) =  Best case complexity.
+
+// Note - >
+// 1. Always consider the worst case for computing complexity of time.
+// 2. Avoid constants they are negligible.
+// 3. Avoid small values that are not gonna affect time complexity
+//  for example - > N^3+N^5 = N^5 .
+
+// Notation - >
+// Big O -> O(variable or constant) = Worst case complexity.
+
+// SPACE COMPLEXITY - > Total space taken by the program to perform 
+// efficiently including auxilliary space and input space.
+
+// Note - >
+// 1. Do not manipulate the input!!.
+
+// Note - > In most servers, it takes 1 second to perform 10^8 
+// operations.
+ 
